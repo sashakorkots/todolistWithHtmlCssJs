f1bfb85acca25ff2467c06510ac7139807fc20bb
Olexander Korkots <k.sasha.g@gmail.com>
2021-04-21 13:17:00 +0300

    added function for add task

--- todolist.js
+++ todolist.js
@@ -1,30 +1,35 @@
 class Task {
-    constructor(id, title, description, dueDate, done) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.done = done;
+    constructor(idOrObject, title, description, dueDate, done) {
+        if (typeof idOrObject === 'object') {
+            Object.assign(this, idOrObject)
+        }
+        else {
+            this.id = idOrObject;
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.done = done;
+        }
     }
 }
 
 let isShowAll = false;
 const todolistElement = document.getElementById('todolist')
 function appendTask(task) {
-    
+
     todolistElement.appendChild(BuildHtmlTask(task));
 }
 function BuildHtmlTask(task) {
-    const {id, title, description, dueDate, done } = task;
+    const { id, title, description, dueDate, done } = task;
     let listElement = document.createElement('section');
     listElement.setAttribute("id", `taskId=${id}`)
-    if (isShowAll && done){
+    if (isShowAll && done) {
         listElement.setAttribute("class", "show-not-all-task");
     }
     let htmlTags =
         `<h3 ${IsComplete(done)}>${title}</h3>` +
         '<div class="checkbox">' +
-            `<input type="checkbox" name="done" value="" ${IsCheck(done)}></input>` +
+        `<input type="checkbox" name="done" value="" ${IsCheck(done)}></input>` +
         '</div>' +
         WriteDescription(description) +
         WriteDueDate(dueDate) +
@@ -37,13 +42,13 @@ function BuildHtmlTask(task) {
 
 function RemoveTask(event) {
     let index = FindById(this.id.split('=')[1]);
-    if (event.target.tagName === 'BUTTON') {        
-        
+    if (event.target.tagName === 'BUTTON') {
+
         todoList.splice(index, 1)
         this.remove();
     }
-    else if (event.target.tagName === 'INPUT') { 
-        todoList[index].done = event.target.checked;  
+    else if (event.target.tagName === 'INPUT') {
+        todoList[index].done = event.target.checked;
         todolistElement.replaceChild(BuildHtmlTask(todoList[index]), this)
     }
 }
@@ -89,7 +94,7 @@ function FindById(id) {
         if (id == todoList[index].id) {
             return index;
         }
-        
+
     }
 }
 
@@ -98,27 +103,27 @@ function FindById(id) {
 
 let buttonElement = document.querySelector("header");
 function AddButtonForShow() {
-    
-    buttonElement.appendChild(ButtonTask());
+
+    buttonElement.appendChild(ButtonShowTask());
 }
 
-function ButtonTask() {
-    
+function ButtonShowTask() {
+
     let button = document.createElement('button');
     if (isShowAll) {
-        button.innerText += 'show all tasks';  
+        button.innerText += 'show all tasks';
     }
     else {
         button.innerText += 'show only no done task';
     }
     button.onclick = ShowAllTask;
-    
+
     return button;
 }
 function ShowAllTask(event) {
     isShowAll = !isShowAll;
-    buttonElement.replaceChild(ButtonTask(isShowAll), this);
-    todolistElement.innerHTML='';
+    buttonElement.replaceChild(ButtonShowTask(isShowAll), this);
+    todolistElement.innerHTML = '';
     todoList.forEach(appendTask);
 
 }
@@ -127,21 +132,70 @@ function ShowAllTask(event) {
 
 
 
+const taskForm = document.getElementById('add-task');
+
+function AddButtonAddTask() {
+
+    buttonElement.appendChild(ButtonAddTask());
+}
+
+function ButtonAddTask() {
+
+    let button = document.createElement('button');
+    button.innerText += 'Add task';
+    button.onclick = CreateForm;
+
+    return button;
+}
+
+
+
+function CreateForm(event) {
+    let form = document.createElement('form');
+    form.setAttribute("name","task");
+    form.setAttribute("class", "form-for-task");
+    form.innerHTML += '<h3><input type="text" name="title" placeholder="Title"></h3>' +
+        '<input type="text" name="description" placeholder="Description">' +
+        '<input type="date" name="dueDate" placeholder="DueDate">' +
+        '<div>' +
+            '<button type="submit">Save new task</button>' +
+        '</div>' ;
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let taskFormData = new FormData(taskForm)
+        let task = Object.fromEntries(taskFormData.entries());
+        task.id = todoList.length;
+        task.done = false
+        let currentTask = new Task(task);
+        todoList.push(currentTask);
+        appendTask(currentTask);
+        console.log(todoList);
+        taskForm.reset();
+    });
+    console.log(form);
+    taskForm.appendChild(form);
+}
+
+
+
+
 
 
 let todoList = [
-    new Task(0,'Make class', '', '2021.02.19', true),
-    new Task(1,'Make constructor', 'by video', '2021.05.19', false),
-    new Task(2,'Listen lesson', '', '2021.05.19', false),
-    new Task(3,'Make object', 'in js', '2021.02.19', true),
-    new Task(4,'Make function', 'for example arrow function', '2021.05.19', false),
-    new Task(5,'Listen lesson', '', '2021.05.19', false),
+    new Task(0, 'Make class', '', '2021.02.19', true),
+    new Task(1, 'Make constructor', 'by video', '2021.05.19', false),
+    new Task(2, 'Listen lesson', '', '2021.05.19', false),
+    new Task(3, 'Make object', 'in js', '2021.02.19', true),
+    new Task(4, 'Make function', 'for example arrow function', '2021.05.19', false),
+    new Task(5, 'Listen lesson', '', '2021.05.19', false),
 ];
 
 
 AddButtonForShow(isShowAll);
+AddButtonAddTask();
 
-todoList.forEach(appendTask, isShowAll);
+todoList.forEach(appendTask);
 
 
 
